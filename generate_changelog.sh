cbe4b4b3c6d145ad58c68ff78b57772df3618364
msyb <musuyaba@gmail.com>
2023-12-19 14:00:14 +0700

    ci: Update release notes

---
+++ generate_changelog.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $(git tag -l | wc -l) -ge 2 ]; then
+    latest_tag=$(git describe --tags --abbrev=0);
+    second_latest_tag=$(git describe --tags --abbrev=0 $latest_tag^);
+    git log --pretty=format:"%s" $second_latest_tag..$latest_tag >> release_notes.md;
+else
+    git log --pretty=format:"%s" $(git rev-list --max-parents=0 HEAD)..HEAD >> release_notes.md;
+fi
