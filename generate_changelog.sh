6ddef0e5a2b5f185f1eac01f04dd02e33ae8eaf2
msyb <musuyaba@gmail.com>
2023-12-19 16:58:55 +0700

    ci: Update generate_changelog.sh script

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -6,8 +6,8 @@ num_tags=$(git tag -l | wc -l)
 echo "Number of tags: $num_tags"
 
 if [ $(git tag -l | wc -l) -ge 2 ]; then
-    latest_tag=$(git describe --tags --abbrev=0);
-    second_latest_tag=$(git describe --tags --abbrev=0 $latest_tag^);
+    latest_tag=$(git describe --tags --abbrev=0)
+    second_latest_tag=$(git describe --tags --abbrev=0 HEAD~1)
     git log --pretty=format:"%s" $second_latest_tag..$latest_tag >> release_notes.md;
 else
     git log --pretty=format:"%s" $(git rev-list --max-parents=0 HEAD)..HEAD >> release_notes.md;
