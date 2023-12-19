19f159415f9e1ab882389ffea8292fbffad220d6
msyb <musuyaba@gmail.com>
2023-12-19 15:07:06 +0700

    ci: Add debug step to list files

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+touch release_notes.md;
+
 if [ $(git tag -l | wc -l) -ge 2 ]; then
     latest_tag=$(git describe --tags --abbrev=0);
     second_latest_tag=$(git describe --tags --abbrev=0 $latest_tag^);
