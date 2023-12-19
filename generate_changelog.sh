093c4d42f0057a2d73884b92340100a4fbf0c7d7
msyb <musuyaba@gmail.com>
2023-12-19 14:16:11 +0700

    ci(debug): Add git log command to generate release notes

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -4,6 +4,8 @@ if [ $(git tag -l | wc -l) -ge 2 ]; then
     latest_tag=$(git describe --tags --abbrev=0);
     second_latest_tag=$(git describe --tags --abbrev=0 $latest_tag^);
     git log --pretty=format:"%s" $second_latest_tag..$latest_tag >> release_notes.md;
+    git log --pretty=format:"%s" $second_latest_tag..$latest_tag;
 else
     git log --pretty=format:"%s" $(git rev-list --max-parents=0 HEAD)..HEAD >> release_notes.md;
+    git log --pretty=format:"%s" $(git rev-list --max-parents=0 HEAD)..HEAD;
 fi
